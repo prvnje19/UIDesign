c62e7fc4c87661c2a8929ac32adb3125e4939b8d
Prvnje19 <prvnje19@gmail.com>
2023-03-04 00:09:06 -0800

    subnavbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SubNavbarComponent } from './component/sub-navbar/sub-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomePageComponent
+    HomePageComponent,
+    SubNavbarComponent
   ],
   imports: [
     BrowserModule,
