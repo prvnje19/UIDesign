29e25daba34f1945e7e1935745d9f0256b0b20e7
arundevu <arunitech09@gmail.com>
2023-03-04 19:28:46 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TableComponent } from './pages/table/table.component';
 
 const routes: Routes = [ 
-
+  { path: 'home', component: HomePageComponent },
+  { path: 'table', component: TableComponent }
 ];
 
 @NgModule({
