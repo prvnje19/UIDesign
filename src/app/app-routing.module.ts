c80b2976bf033d1c64a4f00ad1f0a8f340bd908d
arundevu <arunitech09@gmail.com>
2023-03-04 19:23:48 +0530

    table-change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { TableComponent } from './pages/table/table.component';
 
 const routes: Routes = [ 
-  { path: 'home', component: HomePageComponent },
-  { path: 'table', component: TableComponent }
+
 ];
 
 @NgModule({
