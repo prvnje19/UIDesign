8863f4c1698c908a63d00b8d3f694bd5c1a6e429
Prvnje19 <prvnje19@gmail.com>
2023-03-06 05:42:52 -0800

    added changes

--- src/app/component/sub-navbar/sub-navbar.component.ts
+++ src/app/component/sub-navbar/sub-navbar.component.ts
@@ -1,11 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sub-navbar',
   templateUrl: './sub-navbar.component.html',
   styleUrls: ['./sub-navbar.component.css']
 })
-export class SubNavbarComponent {
+export class SubNavbarComponent implements OnInit {
+ 
   @Input() compName:any;
+  headerName!:string;
+  ngOnInit(): void {
+    this.headerName = "Create a new order"
+  }
+
+
+  handleBack(){
+    console.log("black");
+    // switch (this.pageName) {
+    //   case "":
+      
+    //     break;
+    
+    //   default:
+    //     break;
+    // }
+    
+  }
 
 }
